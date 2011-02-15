f632fdf496375c5071011b80edc009281ae2d968
bauerj <bauerj@omap.(none)>
2011-02-15 14:13:02 +0100

    repaired a bit

--- views.js
+++ views.js
@@ -11,9 +11,7 @@ views.index = function(req,res,matches){
 <title>8080 - Dateien</title>\n \
 <meta name="keywords" content="" />\n \
 <meta name="description" content="" />\n \
-<html><head><link rel="stylesheet" type="text/css" href="/style.css"></head><body><h1>OTR-Mirror</h1>\
-<h2>Download kaputt?!</h2><p>Nein. Wenn bei dir ein Download nicht startet, dann ist das normal so. Dieser Server arbeitet mit einer besonderen Warteschlangen-Art. Starte einfach den von dir gewünschten Download. Falls dein Download-Manager anzeigt, dass dieser nicht aktiv ist (0kB/s) bedeutet es, dass du in der Warteschlange bist. Sobald wieder Bandbreite zur verfügung steht, wird der Download automatisch gestartet. Das spart dir Klicks und sorgt dafür, dass jeder schnell zu seinem Download kommt.</p>\
-<h2>Wo sind die Banner?</h2><p>Dies ist eine freie Mirror Implementation. Was bedeutet "frei"? Frei bedeutet, dass du dir den Source-Code einfach von <a href="http://github.com/niklasfi/8080">github.com</a> herunterladen und auf deinem Server oder Rechner installieren kannst. So wird es jedem ermöglicht die Bandbreite der OTR-Mirror Wolke zu vergrößern. (Patches zum Code sind natürlich auch höchst erwünscht)</p>\
+<html><head><link rel="stylesheet" type="text/css" href="/style.css"></head><body>\
 </head>\n \
 <body>\n \
 <div id="wrapper">\n \
